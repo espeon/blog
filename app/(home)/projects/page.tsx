561ddf8ef4e3083bd16d35ba99d148f525eb875f
natalie <22222885+espeon@users.noreply.github.com>
2024-10-09 11:54:59 -0500

    Added link to muse landing page

--- app/(home)/projects/page.tsx
+++ app/(home)/projects/page.tsx
@@ -28,6 +28,10 @@ export default function Projects() {
             linkText: "GitHub",
             LinkIcon: FaGithub,
           },
+          {
+            link: "https://muse.lutea.co",
+            linkText: "Website"
+          }
         ]}
       />
       <ProjectCard
@@ -40,10 +44,6 @@ export default function Projects() {
             linkText: "GitHub",
             LinkIcon: FaGithub,
           },
-          {
-            link: "https://muse.lutea.co",
-            linkText: "Website"
-          }
         ]}
       />
       <ProjectCard
